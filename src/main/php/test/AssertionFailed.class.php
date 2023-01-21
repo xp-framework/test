1c2e292235a6d742852dff21010aed33d4b75ced
Timm Friebe <thekid@thekid.de>
2023-01-21 19:47:16 +0100

    Omit 1st element, this is always inside test.assert.Assertable

--- src/main/php/test/AssertionFailed.class.php
+++ src/main/php/test/AssertionFailed.class.php
@@ -6,5 +6,8 @@ class AssertionFailed extends XPException {
 
   public function __construct($assertion) {
     parent::__construct('Failed asserting that '.$assertion);
+
+    // Omit 1st element, this is always inside test.assert.Assertable
+    array_shift($this->trace);
   }
 }
